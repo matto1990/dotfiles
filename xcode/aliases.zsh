ab234522d8bf272f1396458456a50ce0252818a8
Matt Oakes <matt@ribot.co.uk>
2014-10-20 21:40:37 +0100

    Added the correct zsh setup

--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -1,1 +1,13 @@
+#xc function courtesy of http://gist.github.com/subdigital/5420709
+function xc {
+  xcode_proj=`ls | grep "\.xc" | sort -r | head -1`
+  if [[ `echo -n $xcode_proj | wc -m` == 0 ]]
+  then
+    echo "No xcworkspace/xcodeproj file found in the current directory."
+  else
+    echo "Found $xcode_proj"
+    open "$xcode_proj"
+  fi
+}
+
 alias ios="open /Applications/Xcode.app/Contents/Applications/iOS\ Simulator.app"
---
+++ zsh/install.sh
@@ -0,0 +1,4 @@
+if [ -n "$ZSH_VERSION" ]; then
+	echo "   Installing oh-my-zsh for you."
+	curl -L http://install.ohmyz.sh | sh
+fi
