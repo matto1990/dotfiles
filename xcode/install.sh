8c18a6c1e6d69fd64df55ffb4ceefb0e853f5d66
Matt Oakes <matt@ribot.co.uk>
2014-10-21 00:32:59 +0100

    Added alcatraz to the xcode topic

--- xcode/install.sh
+++ xcode/install.sh
@@ -3,3 +3,9 @@ then
 	echo "  Installing cocoapods for you."
 	gem install cocoapods
 fi
+
+if [ ! -d "$HOME/Library/Application Support/Developer/Shared/Xcode/Plug-ins/Alcatraz.xcplugin" ]
+then
+    echo "   Installing alcatraz for you."
+    curl -fsSL https://raw.github.com/supermarin/Alcatraz/master/Scripts/install.sh | sh
+fi
