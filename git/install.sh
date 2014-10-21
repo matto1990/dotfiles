3eae31a2e21813a782ceb834d3e446d8a02e615d
Matt Oakes <matt@ribot.co.uk>
2014-10-21 10:10:33 +0100

    Added some extra git homebrew files

--- git/install.sh
+++ git/install.sh
@@ -3,5 +3,11 @@
 if [ "$(which git)" != "$(brew --prefix)/bin/git" ]
 then
   echo "  Installing git from homebrew."
-  brew install git
+  brew install git tig
+fi
+
+if test ! $(which hub)
+then
+  echo "  Installing hub from homebrew."
+  brew install --HEAD hub
 fi
